bd9b3cb04dea50e52bf6e42e626189e3b14d86c1
generic-keith <keith.staines.ext@bbc.co.uk>
2023-10-01 07:10:20 +0100

    Initial Commit

---
+++ CoinGecko/CoinGeckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGeckoApp.swift
+//  CoinGecko
+//
+//  Created by Keith Staines on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinGeckoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
